653afc918419258042316201aced488da9b15895
phoenixofficial7 <103618189+phoenixofficial7@users.noreply.github.com>
2023-04-07 08:22:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "AQBtn4EAazDgWzlZ-KRdkoeB1bAj3SsCS_PsXtoAHzYm1opq4LXEpnLsnMDI1Pa3iSvgVTgkNi-Jxa5NWz3y286Ub9wtxwbsG9HZ--8D3fjsBFfAmb_86t2EWhD8EoGQr30w1x5QZPgUJk91dOm6zGnStW1B6GqrpPWAUEY6ChHxgn-LB9U0Kk0qzd51xG1UO8MN2qczsDoV7CdHov_MUM1bGIVqzFxeItI0E_tNwKmFFVMP4UwnxOqVOyAWD3f1Y6nRk6WO3zpQd-YTWDPQ_EAJEXhtPHafb3tn_z-MySsql-UvqEhzavDCkAdPtqJv0hn54mUyF8Y0dBZJ-zDWDqbX8FFuwgAAAAE3HbfHAA")
 
 
 bot = Client(
